af10216c0bf007fa569d39d8e4a04ee771086dc2
kalgcny09 <kalgcny@gmail.com>
2017-05-24 11:55:38 -0400

    added Movie Project

---
+++ movie/js/scripts.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+	const apiBaseUrl = 'http://api.themoviedb.org/3';
+	const imageBaseUrl = 'http://image.tmdb.org/t/p/';
+	const nowPlayingUrl = apiBaseUrl + '/movie/now_playing?api_key=' + apiKey;
+	$.getJSON(nowPlayingUrl, (nowPlayingData)=>{
+	
+		var nowPlayingHTML = '';
+		for(let i =0; i < nowPlayingData.results.length; i++){
+			var posterUrl = imageBaseUrl + 'w300' + nowPlayingData.results[i].poster_path;
+			nowPlayingHTML += '<div class="col-sm-3">';
+				nowPlayingHTML += `<img src="${posterUrl}">`;
+			nowPlayingHTML +=`</div>`;
+		}
+		$('#movie-grid').html(nowPlayingHTML);
+		
+	})
+
+	$('#movie-form').submit((event)=>{
+		event.preventDefault();
+		var userInput = $('#search-input').val();
+		$('#search-input').val('');
+		var safeUserInput = encodeURI(userInput);
+		var searchUrl = apiBaseUrl + '/search/movie?query='+ safeUserInput + '&api_key=' +apiKey;
+		$.getJSON(searchUrl, (searchMovieData)=>{
+			var searchMovieHTML = getHTML(searchMovieData)
+			$('#movie-grid').html(searchMovieHTML);
+
+
+		})
+	})
+
+	function getHTML(data) {
+	var newHTML = '';
+		for(let i =0; i < data.results.length; i++){
+			var posterUrl = imageBaseUrl + 'w300' + data.results[i].poster_path;
+			nowPlayingHTML += '<div class="col-sm-3">';
+				nowPlayingHTML += `<img src="${posterUrl}">`;
+			nowPlayingHTML +=`</div>`;
+		}
+
+	return newHTML
+	}
+})
+
